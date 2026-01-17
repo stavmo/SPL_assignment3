226fb7d2a34d515b40400e39e324528809d9e3a0
stavmo <stavmo@post.bgu.ac.il>
2026-01-17 12:00:06 +0000

    fixed handleAccept and continueRead to work with stomp

--- server/src/main/java/bgu/spl/net/srv/NonBlockingConnectionHandler.java
+++ server/src/main/java/bgu/spl/net/srv/NonBlockingConnectionHandler.java
@@ -1,7 +1,7 @@
 package bgu.spl.net.srv;
 
 import bgu.spl.net.api.MessageEncoderDecoder;
-import bgu.spl.net.api.MessagingProtocol;
+//import bgu.spl.net.api.MessagingProtocol;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -9,13 +9,14 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import bgu.spl.net.api.StompMessagingProtocol;
 
 public class NonBlockingConnectionHandler<T> implements ConnectionHandler<T> {
 
     private static final int BUFFER_ALLOCATION_SIZE = 1 << 13; //8k
     private static final ConcurrentLinkedQueue<ByteBuffer> BUFFER_POOL = new ConcurrentLinkedQueue<>();
-
-    private final MessagingProtocol<T> protocol;
+    private final StompMessagingProtocol<T> protocol;
+    //private final MessagingProtocol<T> protocol;
     private final MessageEncoderDecoder<T> encdec;
     private final Queue<ByteBuffer> writeQueue = new ConcurrentLinkedQueue<>();
     private final SocketChannel chan;
@@ -23,7 +24,7 @@ public class NonBlockingConnectionHandler<T> implements ConnectionHandler<T> {
 
     public NonBlockingConnectionHandler(
             MessageEncoderDecoder<T> reader,
-            MessagingProtocol<T> protocol,
+            StompMessagingProtocol<T> protocol,
             SocketChannel chan,
             Reactor reactor) {
         this.chan = chan;
@@ -49,14 +50,15 @@ public class NonBlockingConnectionHandler<T> implements ConnectionHandler<T> {
                     while (buf.hasRemaining()) {
                         T nextMessage = encdec.decodeNextByte(buf.get());
                         if (nextMessage != null) {
-                            T response = protocol.process(nextMessage);
-                            if (response != null) {
-                                writeQueue.add(ByteBuffer.wrap(encdec.encode(response)));
-                                reactor.updateInterestedOps(chan, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
-                            }
+                            protocol.process(nextMessage);
+                            //T response = protocol.process(nextMessage);
+                            //if (response != null) {
+                            //    writeQueue.add(ByteBuffer.wrap(encdec.encode(response)));
+                            //    reactor.updateInterestedOps(chan, SelectionKey.OP_READ | SelectionKey.OP_WRITE);
                         }
                     }
-                } finally {
+                }
+                 finally {
                     releaseBuffer(buf);
                 }
             };
--- server/src/main/java/bgu/spl/net/srv/Reactor.java
+++ server/src/main/java/bgu/spl/net/srv/Reactor.java
@@ -1,7 +1,7 @@
 package bgu.spl.net.srv;
 
 import bgu.spl.net.api.MessageEncoderDecoder;
-import bgu.spl.net.api.MessagingProtocol;
+//import bgu.spl.net.api.MessagingProtocol;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.ClosedSelectorException;
@@ -12,11 +12,14 @@ import java.nio.channels.SocketChannel;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
+import bgu.spl.net.api.StompMessagingProtocol;
+//import bgu.spl.net.impl.stomp.StompProtocol;
 
 public class Reactor<T> implements Server<T> {
 
     private final int port;
-    private final Supplier<MessagingProtocol<T>> protocolFactory;
+    //private final Supplier<MessagingProtocol<T>> protocolFactory;
+    private final Supplier<StompMessagingProtocol<T>> protocolFactory;
     private final Supplier<MessageEncoderDecoder<T>> readerFactory;
     private final ActorThreadPool pool;
     private Selector selector;
@@ -30,7 +33,7 @@ public class Reactor<T> implements Server<T> {
     public Reactor(
             int numThreads,
             int port,
-            Supplier<MessagingProtocol<T>> protocolFactory,
+            Supplier<StompMessagingProtocol<T>> protocolFactory,
             Supplier<MessageEncoderDecoder<T>> readerFactory) {
 
         this.pool = new ActorThreadPool(numThreads);
@@ -102,16 +105,16 @@ public class Reactor<T> implements Server<T> {
 
         int connectionId = nextId.getAndIncrement(); //
 
-        //StompMessagingProtocol<T> protocol = protocolFactory.get();  //will implement after STOMP is implemented
-        //protocol.start(connectionId, connections);
-
+        StompMessagingProtocol<T> protocol = protocolFactory.get();  //
+        
         final NonBlockingConnectionHandler<T> handler = new NonBlockingConnectionHandler<>(
                 readerFactory.get(),
-                protocolFactory.get(),
+                protocol,
                 clientChan,
                 this);
 
         connections.connect(connectionId, handler); ///
+        protocol.start(connectionId, connections); // makes sure start is called before process 
 
         clientChan.register(selector, SelectionKey.OP_READ, handler);
     }
